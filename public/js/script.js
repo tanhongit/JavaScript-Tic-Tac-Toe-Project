1a9f958ea14a5eae9705266f62db54e125b8a3c7
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-08 13:16:53 +0700

    create startgame function

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+    document.querySelector(".endgame").style.display = "none";
+    origBoard = Array.from(Array(9).keys());
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
+    }
+}
